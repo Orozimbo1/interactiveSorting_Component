555634c470d57e6b5e4b87bd3d5e2a3d03b98823
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-06-20 22:57:38 -0300

    Logica em js do componente pronta

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('oi')
+const ratings = document.querySelectorAll('li');
+const submit = document.querySelector('#submit')
+const start = document.querySelector('#start')
+const end = document.querySelector('#end')
+const span = document.querySelector('span')
+
+let rate;
+
+const removeClassActive = () => {
+  for(let i = 0; i < ratings.length; i++) {
+    if(ratings[i].classList.contains('active')) ratings[i].classList.remove('active')
+  }
+}
+
+for(let i = 0; i < ratings.length; i++) {
+  ratings[i].addEventListener('click', function() {
+    removeClassActive()
+    rate = this.textContent
+    this.classList.add('active')
+  })
+}
+
+submit.addEventListener('click', () => {
+  !rate && alert('You have to choose a rating')
+  start.classList.add('hidden')
+  span.innerText = rate
+  end.classList.add('show')
+})
+
+// removeClass()
